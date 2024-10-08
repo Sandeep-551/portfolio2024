c8ad085c0897e3fdd4a363ee36ab5a3f23be82f1
Sandeep R <68626582+Sandeep-551@users.noreply.github.com>
2024-10-08 14:08:44 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Sandeep-551",
   resume:
-    "https://drive.google.com/file/d/1roXhXcdSJjOPH4W_mbLJbPKthQt4QvNE/view?usp=sharing",
+    "https://drive.google.com/file/d/1erK2YuY7aRRgeswVdXdXWHg718URsPR-/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/sandeep-r-2a1ab9237/",
   Email: "mailto:sandeep0511r@gmail.com"
 
